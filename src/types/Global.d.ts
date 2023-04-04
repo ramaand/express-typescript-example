b4ac01278b289cf527f8bd2756be74bf660fc85b
ramaand <gilangramadhanii191@gmail.com>
2023-04-04 11:33:26 +0700

    chore: add types

--- src/types/Global.d.ts
+++ src/types/Global.d.ts
@@ -3,3 +3,10 @@ import { Document } from 'mongoose';
 interface DocumentResult extends Document {
   _doc?: any;
 }
+
+interface IErrorResponse {
+  success: boolean;
+  status: number;
+  message: string;
+  stack?: any;
+}
--- src/types/Route.d.ts
+++ src/types/Route.d.ts
@@ -1,4 +1,4 @@
-interface IRoute {
+export interface ICustomRoute {
   url: string;
   service: any;
 }
