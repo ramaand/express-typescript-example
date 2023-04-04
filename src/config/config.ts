d5dd95bc849cfe03da1ce1e37ece406d35f104ae
ramaand <gilangramadhanii191@gmail.com>
2023-04-04 11:42:55 +0700

    fix: mongo url

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 const MONGO_USERNAME = process.env.MONGO_USERNAME! || '';
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD! || '';
-const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.hom8sav.mongodb.net/booking`;
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@bookapp.zpq5za7.mongodb.net/`;
 
 const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT!) : 8800;
 
